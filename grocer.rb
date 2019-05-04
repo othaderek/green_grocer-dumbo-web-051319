5b6412c27beefd75c341ae3ee531825adb386aca
Otha D Hernandez <otha.hernandez@gmail.com>
2019-05-04 18:58:50 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,7 +1,13 @@
 def consolidate_cart(cart)
   new_hash = {}
   cart.each do |item| # Here we are iterating into the array that contains the hashes 
-    item.each do |item_name, item_|
-  
-  
+    item.each do |item_name, item_hash|
+      if new_hash.key?(item_name) == false 
+        new_hash[item_name] = item_hash
+        new_hash[item_name][:count] = 1 
+      else
+        new_hash[item_name][:count] += 1 
+      end
+    end
+  end
 end
